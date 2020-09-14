9e56715a9337eb550334d06e266f4637d047629a
Jasmin Ziegler <zieglerja53524@th-nuernberg.de>
2020-09-14 18:51:55 +0200

    added doc

--- obvision/reconstruct/space/SensorPolar3DBase.cpp
+++ obvision/reconstruct/space/SensorPolar3DBase.cpp
@@ -1,80 +1,158 @@
 #include "SensorPolar3DBase.h"
+#include "obcore/base/System.h"
 #include "obcore/math/mathbase.h"
-
+#include <limits>
 
 namespace obvious
 {
 
-SensorPolar3DBase::SensorPolar3DBase(double inclMin, double inclMax, double inclRes, double azimMin, double azimMax, 
-double azimRes, double maxRange, double minRange, double lowReflectivityRange)
-: Sensor(3, maxRange, minRange, lowReflectivityRange)
+SensorPolar3DBase::SensorPolar3DBase(unsigned int raysIncl, double inclMin, double inclMax, double inclRes, double azimMin, double azimMax, double azimRes,
+                                     double maxRange, double minRange, double lowReflectivityRange)
+    : Sensor(3, maxRange, minRange, lowReflectivityRange)
 {
-    
-    ///////////ACHTUNG ICH KRIEG HIER EIN PROBLEM! raysINCL IST HIER 15 und nicht 16!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //!!!!!!!!!!!!!!!!!!!!! ich mach jetzt bei beiden +1 weil ich nicht nur 15 raysIncl hab sondern 16 und 
-    //!!! weil bei azimuth ich den 0° und 360° ray haben will? idk. testen
-    unsigned int raysIncl = round(static_cast<unsigned>((abs(inclMin) + abs(inclMax)) / inclRes)) + 1;
-    unsigned int raysAzim = round(static_cast<unsigned>(2 * M_PI / azimRes)) + 1;
+  _inclRes                  = inclRes;
+  _inclMin                  = inclMin;
+  _inclMax                  = inclMax;
+  _inclSpan                 = _inclRes * (static_cast<double>(raysIncl) - 1); // @todo CHECK FOR OTHER MODELS
+  _inclNegSpan              = abs(inclMin);
+  _azimRes                  = azimRes;
+  _azimMin                  = azimMin;
+  const double resetInclMin = inclMin;
 
-    _azimRes = azimRes;
-    _inclRes = inclRes;
+  int raysAzim = static_cast<int>(round(2 * M_PI / _azimRes));
 
-    // inherited from class Sensor
-    _width = raysAzim;       
-    _height = raysIncl;
-    _size = _width * _height;
-    
-    _data = new double[_size];
-    _mask = new bool[_size];
-    for(unsigned int i = 0; i < _size; i++)
-        _mask[i] = true;
+  // inherited from Sensor
+  _width  = static_cast<unsigned>(raysAzim);
+  _height = static_cast<unsigned>(raysIncl);
+  _size   = _width * _height;
+  _data   = new double[_size];
+  _mask   = new bool[_size];
+  for(unsigned int i = 0; i < _size; i++)
+    _mask[i] = true;
 
-    _rays = new obvious::Matrix(3, _size);
-    _raysLocal = new obvious::Matrix(3, _size);
-    *_raysLocal = *_rays;
-
-    unsigned int n = 0; //count all iterations of inner and outer loop for storing vals in _rays
-    double currIncl = inclMin;  //incremented by inclRes in each loop
-    double currAzim = 0.0;      //azimuth start angle incremented by azimRes in each loop
-
-    //!!!!!!!!!!!!!!!!!!!! < oder <= ?
-    for(unsigned int i = 0; i < _width; i++)
+  // set index map
+  obvious::System<int>::allocate(_width, _height, _indexMap);
+  for(unsigned int row = 0; row < _width; row++) // AZIM
+  {
+    for(unsigned int column = 0; column < _height; column++) // INCL
     {
-        for(unsigned int j = 0; j < _height; j++, n++)
-        {
-            obvious::Matrix calcRay(3, 1);
-
-            //to follow inclination angle theta in spherical coordinate system (from y-axis down)
-            double theta = 0.0;
-            const double piHalf = deg2rad(90.0);
-
-            if(currIncl < 0)
-            {
-                theta = piHalf + currIncl * (-1);
-            }
-            else
-            {
-                theta = piHalf - currIncl;
-            }
-
-            calcRay(0, 0) = sin(theta) * cos(currAzim);
-            calcRay(1, 0) = sin(theta) * sin(currAzim);
-            calcRay(2, 0) = cos(theta);
-            
-        }
+      _indexMap[row][column] = row * _height + column;
     }
+  }
 
+  _rays    = new Matrix(3, _size);
+  Matrix R = Matrix(*_T, 0, 0, 3, 3);
+
+  unsigned int n           = 0;
+  double       currentAzim = 0.0;
+
+  for(unsigned int i = 0; i < _width; i++)
+  {
+    currentAzim = _azimMin + i * _azimRes;
+
+    for(unsigned int j = 0; j < _height; j++, n++)
+    {
+      Matrix       calcRay(3, 1);
+      double       thetaSphere = 0.0;
+      const double piHalf      = deg2rad(90.0);
+      // hier muss ich die inclination Winkel in thetaSphzere jetzt umrechnen, weil ich meinen Fächer ja von _inclMin bis _inclMax aufbaue und die
+      // Kugelkoord. theta von der z-Achse runter auf die x-y-Ebene definieren
+      if(inclMin < 0.0)
+
+      {
+        thetaSphere = piHalf + inclMin * (-1.0);
+      }
+      else
+      {
+        thetaSphere = piHalf - inclMin;
+      }
+
+      calcRay(0, 0) = sin(thetaSphere) * cos(currentAzim);
+      calcRay(1, 0) = sin(thetaSphere) * sin(currentAzim);
+      calcRay(2, 0) = cos(thetaSphere);
+
+      const double length    = sqrt(calcRay(0, 0) * calcRay(0, 0) + calcRay(1, 0) * calcRay(1, 0) + calcRay(2, 0) * calcRay(2, 0));
+      const double lengthInv = 1.0 / length;
+      calcRay                = R * calcRay;
+
+      (*_rays)(0, n) = calcRay(0, 0) * lengthInv;
+      (*_rays)(1, n) = calcRay(1, 0) * lengthInv;
+      (*_rays)(2, n) = calcRay(2, 0) * lengthInv;
+
+      inclMin += _inclRes;
+    }
+    inclMin = resetInclMin;
+  }
+
+  _raysLocal  = new Matrix(3, _size);
+  *_raysLocal = *_rays;
 }
 
 SensorPolar3DBase::~SensorPolar3DBase()
 {
-    std::cout << "destruction baby" << std::endl;
+  delete[] _data;
+  delete[] _mask;
+  System<int>::deallocate(_indexMap);
+  delete _rays;
+  delete _raysLocal;
 }
 
-// M sind die Koordinaten des TSD SPACES! von allen VOXELN die Mittelpunkte!
 void SensorPolar3DBase::backProject(obvious::Matrix* M, int* indices, obvious::Matrix* T)
 {
+  Matrix PoseInv = getTransformation();
+  PoseInv.invert();
+  if(T)
+    PoseInv *= *T;
 
+  Matrix coords3D = Matrix::multiply(PoseInv, *M, false, true);
+
+  double inclAngle       = 0.0;
+  int    lookupInclIndex = 0;
+
+  for(unsigned int i = 0; i < M->getRows(); i++)
+  {
+    double length    = sqrt(coords3D(0, i) * coords3D(0, i) + coords3D(1, i) * coords3D(1, i) + coords3D(2, i) * coords3D(2, i));
+    double lengthInv = 1.0 / length;
+
+    double theta = acos(coords3D(2, i) * lengthInv);
+
+    // theta in inclAngle umwandeln vor Indexberechnung
+    if(theta >= deg2rad(90.0))
+    {
+      inclAngle = -(theta - deg2rad(90.0)); // for all angles from -15° to 0°
+    }
+    else
+    {
+      inclAngle = deg2rad(90.0) - theta; // for all angles bigger than 0° to 15°
+    }
+
+    double azimAngle = atan2(coords3D(1, i), coords3D(0, i)) + M_PI; // +PI because atan2 defines angles in 3rd and 4th quadrant clockwise = negatively. By
+                                                                     // adding +pi, all results are positive -> index calculation works
+
+    // leave current loop if inclAngle out of vertical aperture/measurement area between _inclMin and _inclMax; set current index = -1 (invalid)
+    if((inclAngle < _inclMin) || (inclAngle > _inclMax))
+    {
+      indices[i] = -1;
+      continue;
+    }
+    else
+    {
+      // shift inclAngle to positive 1st quadrant before index calculations so its easier to calculate indices with resolution vals
+      double inclShifted = inclAngle + _inclNegSpan;
+      int    azimIndex   = static_cast<int>(floor(azimAngle / _azimRes));
+
+      // ohne LOOKUPINDEX --> testdata
+      int inclIndex = static_cast<int>(floor(inclShifted / _inclRes));
+      int idxCheck  = azimIndex * static_cast<int>(_height) + inclIndex;
+      indices[i]    = _indexMap[azimIndex][inclIndex];
+
+      /** mit LOOKUP --> realdata
+      // int inclIndex = static_cast<int>(floor(inclShifted / _inclRes));
+      // lookupInclIndex = lookupIndex(inclIndex);
+      // indices[i] = _indexMap[azimIndex][lookupInclIndex];
+      **/
+    }
+  }
 }
 
 } // namespace obvious
--- obvision/reconstruct/space/SensorPolar3DBase.h
+++ obvision/reconstruct/space/SensorPolar3DBase.h
@@ -1,37 +1,47 @@
-#ifndef OBVISION_RECONSTRUCT_SPACE_SENSORPOLAR3DBASE_H_
-#define OBVISION_RECONSTRUCT_SPACE_SENSORPOLAR3DBASE_H_
-
 #include "obcore/math/linalg/eigen/Matrix.h"
 #include "obvision/reconstruct/Sensor.h"
 
 namespace obvious
 {
+
 /**
- * @class SensorPolar3DBase
- * @brief generic sensor model class for polar sensing units (to be tested for: Velodyne VLP16 PUCK, Velodyne HDL-32E, ohm_tilt_scanner_3d, SICK LDMRS8, ohm_4d_scanner, Ouster OS0-128, & many more)
- * @brief all polar sensor model classes inherit from this class & implement lookupIndex
- * @brief this class itself inherits basic sensor modalities from class Sensor 
+ * @class SensorVelodyne3D
+ * @brief class for polar 3D laser scanners (VLP16 PUCK, E32) / tilting systems (tilt_3d, tilt4d)
+ * @brief inherit from this class and implement method lookupIndex for each new sensor you want to integrate
  * @author Jasmin Ziegler
  */
 class SensorPolar3DBase : public Sensor
 {
 public:
+  /**
+   * Standard constructor
+   * @param[in] raysIncl number of inclination rays of scanning device (vertical)
+   * @param[in] inclMin lowest inclination angle in RAD
+   * @param[in] inclMax highest inclination angle in RAD
+   * @param[in] inclRes resolution of inclination rays in RAD, i.e. angle between two vertical rays
+   * @param[in] azimMin lowest azimuth angle in RAD
+   * @param[in] azimMax highest azimuth angle in RAD
+   * @param[in] azimRes resolution of azimuth rays in RAD, angle between two horizontal rays in 360° plane
+   * @param[in] maxRange maximum measurement range (inherited from class Sensor)
+   * @param[in] minRange minimum measurement range, i.e. a dead zone (inherited from class Sensor)
+   * @param[in] lowReflectivityRange range for objects with low remission (inherited from class Sensor)
+   */
+  SensorPolar3DBase(unsigned int raysIncl, double inclMin, double inclMax, double inclRes, double azimMin, double azimMax, double azimRes,
+                    double maxRange = INFINITY, double minRange = 0.0, double lowReflectivityRange = INFINITY);
 
-/**
- * Standard Constructor
- * @param[in] inclMin lowest inclination angle [RAD] (vertical)
- * @param[in] inclMax highest inclination angle in [RAD] (vertical)
- * @param[in] inclRes vertical angular resolution [RAD]
- * @param[in] azimMin lowest azimuth angle [RAD] (horizontal)
- * @param[in] azimMax highest azimuth angle in [RAD] (horizontal)
- * @param[in] azimRes horizontal angular resolution [RAD]
- */
-SensorPolar3DBase(double inclMin, double inclMax, double inclRes, double azimMin, double azimMax, double azimRes, double maxRange = INFINITY, double minRange = 0.0, double lowReflectivityRange = INFINITY);
- 
-/**
- * Destructor
- */
-virtual ~SensorPolar3DBase();
+  /**
+   * Destructor
+   */
+  virtual ~SensorPolar3DBase();
+
+  /**
+   * match firing sequence of real sensor data to _indexMap. This generic model builds the _indexMap for each azimuth starting from the lowest inclination
+   * value and incrementing +inclRes until inclMax is reached. Then +azimRes & this is repeated for all azimuths
+   * The real sensor data might come in a differnt order though! I.e. in the case of the Velodyne PUCK VLP16, the firing sequence "lists lasers in the order
+   * they are fired. Though the VLP-16's lasers are organized in a single, vertical column, they are not fired from one end to the other. Instead, the firing
+   * sequence "hops around." This is to avoid "cross-talk" or interference." (see VLP-16 User Manual, page 54ff)
+   */
+  virtual int lookupIndex(int inclIndex) = 0;
 
   /**
    * Project all coordinates (center of each voxel in tsd space) back to sensor index: which sensor ray comes closest to the coordinate?
@@ -39,14 +49,17 @@ virtual ~SensorPolar3DBase();
    * @param[out] indices vector of projection results (must be allocated outside)
    * @param[in] T temporary transformation matrix of coordinates
    */
-void backProject(obvious::Matrix* M, int* indices, obvious::Matrix* T = NULL);
+  void backProject(obvious::Matrix* M, int* indices, obvious::Matrix* T = NULL);
 
 private:
-double _azimRes;
-double _inclRes;
-
+  double _inclRes;
+  double _inclMin;
+  double _inclMax;
+  double _inclSpan;
+  double _inclNegSpan;
+  double _azimRes;
+  double _azimMin;
+  int**  _indexMap;
 };
 
-} /*namespace obvious */
-
-#endif /* OBVISION_RECONSTRUCT_SPACE_SENSORPOLAR3DBASE_H_ */
+} // namespace obvious
